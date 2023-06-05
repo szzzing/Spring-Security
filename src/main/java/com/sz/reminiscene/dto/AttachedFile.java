7a53ba492b6fcf289a02019b9bfbd555ed11ffbd
Sejin Kim <xxszzzing@gmail.com>
2023-06-05 16:55:28 +0900

    build: 첨부파일 관련 설정

---
+++ src/main/java/com/sz/reminiscene/dto/AttachedFile.java
@@ -0,0 +1,70 @@
+package com.sz.reminiscene.dto;
+
+import java.io.File;
+import java.sql.Date;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AttachedFile {
+	private int fileId;
+	private String fileType;
+	private Date fileCreateDate;
+	private String fileName;
+	private String fileModifyName;
+	private String fileLink;
+	private String fileStatus;
+	private String fileThumbnail;
+	
+	public static String[] saveFile(MultipartFile file, HttpServletRequest request) {
+		String root = request.getSession().getServletContext().getRealPath("resources");
+		String savePath = root + "\\uploadFiles";
+		
+		File folder = new File(savePath);
+		if(!folder.exists()) {
+			folder.mkdir();
+		}
+		
+		String uuid = UUID.randomUUID().toString();
+
+		String originFileName = file.getOriginalFilename();
+		String renameFileName = uuid + originFileName.substring(originFileName.lastIndexOf("."));
+		
+		String renamePath = folder + "\\" + renameFileName;
+		
+		try {
+			file.transferTo(new File(renamePath));
+		} catch (Exception e) {
+			System.out.println("파일 업로드 실패" + e.getMessage());
+		}
+		
+		String[] returnArr = new String[2];
+		returnArr[0] = savePath;
+		returnArr[1] = renameFileName;
+		
+		return returnArr;
+	}
+	
+	public static void deleteFile(String fileName, HttpServletRequest request) {
+		String root = request.getSession().getServletContext().getRealPath("resources");
+		String savePath = root + "\\uploadFiles";
+		
+		File f = new File(savePath + "\\" + fileName);
+		if(f.exists()) {
+			f.delete();
+		}
+	}
+}
